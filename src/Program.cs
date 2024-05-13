9169193feff0d2ae45742dbc4f1206965347cddb
JC Castillo <castillochristopherjames@gmail.com>
2024-05-13 16:47:36 +0800

    temporarily disabled https

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,7 @@
 using DotNetEnv;
 using Microsoft.EntityFrameworkCore;
 using SensusAPI.Models.DB;
+using Microsoft.AspNetCore.Authentication.Certificate;
 
 DotNetEnv.Env.Load(".env");
 var builder = WebApplication.CreateBuilder(args);
@@ -8,24 +9,24 @@ var builder = WebApplication.CreateBuilder(args);
 // add services
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
-builder.Services.AddHttpsRedirection(opts => {
-    opts.HttpsPort = 44350;
-    opts.RedirectStatusCode = 301;
-});
+// builder.Services.AddHttpsRedirection(opts => {
+//     opts.HttpsPort = 44350;
+//     opts.RedirectStatusCode = 301;
+// });
 builder.Services.AddControllers();
 builder.Services.AddDbContext<SensusDbContext>(opts => {
     opts.UseNpgsql(DotNetEnv.Env.GetString("PSQL_KEY"));
 });
+// builder.Services.AddAuthentication(
+//         CertificateAuthenticationDefaults.AuthenticationScheme)
+//     .AddCertificate();
 builder.Services.AddScoped<IPollHandler, PollHandler>();    // poll handler
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment()){
-    // use CORS while application is in development
-    app.UseCors();
-}
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
 
-app.UseHttpsRedirection();  // redirect requests to http
+//app.UseHttpsRedirection();  // redirect requests to http
 
 app.UseRouting(); // allow routing
 
